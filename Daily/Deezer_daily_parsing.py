17736d47fa6225d6b975fa41620338b5686c3e07
HumBureau <70520141+HumBureau@users.noreply.github.com>
2020-11-07 15:04:17 +0300

    Deezer now scrapes all artists for each track

--- Daily/Deezer_daily_parsing.py
+++ Daily/Deezer_daily_parsing.py
@@ -6,20 +6,21 @@
 
 # данный скрипт:
 
-# парсит чарт Deezer (https://www.deezer.com/en/playlist/1116189381)
-# периодичность - 20 минут
-# все это нужно потому, что чарт не обновляется в известное фиксированное время
+## парсит чарт Deezer (https://www.deezer.com/en/playlist/1116189381)
+## периодичность - 20 минут 
+### все это нужно потому, что чарт не обновляется в известное фиксированное время
+
+# записываемая дата = "сегодня"
 
 # Время запуска скрипта: 00:01 каждый день
 
 # на выходе:
-# если находит новый чарт, обновляет all_deezer.csv
+## если находит новый чарт, обновляет csv all_deezer
 
 
-# In[5]:
+# In[1]:
 
 
-import os
 import pandas as pd
 import re
 import requests
@@ -33,48 +34,62 @@ from datetime import datetime
 # In[ ]:
 
 
-if os.path.exists("all_deezer.csv") == False:
-    df = pd.DataFrame(columns=['rank', 'title', 'artist', "date"])
-    df.to_csv("all_deezer.csv", encoding="utf-8")
-
 # грузим данные за предыдущий день
 all_deezer = pd.read_csv("all_deezer.csv")
-# удаляем получающуюся после импорта лишнюю колонку
-all_deezer = all_deezer.drop(all_deezer.columns[[0]], axis=1)
-old_df = all_deezer[-100:]
+all_deezer = all_deezer.drop(all_deezer.columns[[0]], axis=1) # удаляем получающуюся после импорта лишнюю колонку 
+
+# на всякий случай чистим от дублей
+all_deezer = all_deezer.drop_duplicates()
+all_deezer.reset_index(inplace=True) 
+all_deezer.drop(all_deezer.columns[[0]], axis=1, inplace=True)
+
+# берем последние 100 строк, чтобы сверить с новыми
+old_df=all_deezer[-900:-800]
 o_l = list(old_df["title"])
-
-
+    
+    
 # базовая ссылка на последний актуальный ежедневный чарт по России
-# ссылка на постоянный плейлист
-request_deezer = requests.get('https://api.deezer.com/playlist/1116189381')
-deezer_chart_json = request_deezer.json()  # через API получаем json
-# выбираем только список треков
-new_df = pd.DataFrame(deezer_chart_json['tracks']['data'])
-# поскольку имена артистов "запакованы", осуществляем распаковку через цикл и заново приклеиваем к датафрейму
-artists_deezer_top = []
-for i in range(0, 100):
-    artists_deezer_top.append(
-        deezer_chart_json['tracks']['data'][i]['artist']['name'])
-new_df['artist'] = artists_deezer_top
-new_df['rank'] = new_df.reset_index().index + 1
+request_deezer = requests.get('https://api.deezer.com/playlist/1116189381') # ссылка на постоянный плейлист
+deezer_chart_json = request_deezer.json() # через API получаем json 
+new_df = pd.DataFrame(deezer_chart_json['tracks']['data']) # выбираем только список треков
+
+# Находим имена ВСЕХ артистов для каждого трека через API трека
+
+A_l = []
+for i in new_df["id"]:
+    api_track = 'https://api.deezer.com/track/'+str(i)
+    request_deezer = requests.get(api_track) 
+    json = request_deezer.json()
+    a_l = []
+    for j in json["contributors"]:
+        a_l.append(j["name"])
+    g_a_l = [i for n, i  in enumerate(a_l) if i not in a_l[:n]] 
+    artists = ", ".join(g_a_l) #  delete duplicate mentions
+    A_l.append(artists)
+new_df["artist"] = A_l
+
+new_df['rank'] = new_df.reset_index().index +1 
 new_df = new_df[['rank', 'title', 'artist']]
-
-n_l = list(new_df["title"])
-
-# новый ли тот чат, который мы запарсили?
-if o_l != n_l:
-
+    
+n_l = list(new_df["title"])   
+        
+# новый ли тот чат, который мы заскрейпили?
+if o_l != n_l:           
+            
     # задаем дату
-    date = datetime.now()
-    new_df["date"] = datetime.strftime(date, "%d/%m/%Y")
-
+    date = datetime.now() 
+    new_df["date"] = datetime.strftime(date,"%d/%m/%Y")  
+            
     # вписываем данные в csv
     frames = [all_deezer, new_df]
     all_deezer = pd.concat(frames, sort=False)
-    all_deezer.to_csv("all_deezer.csv", encoding="utf-8")
-
+    all_deezer.reset_index(inplace=True) 
+    all_deezer.drop(all_deezer.columns[[0]], axis=1, inplace=True)
+    all_deezer.to_csv("all_deezer.csv", encoding = "utf-8")
+    
     print("New Deezer chart is found. No more scraping for today!")
-
+            
 else:
+    all_deezer.to_csv("all_deezer.csv", encoding = "utf-8") # сохраняем на всякий случай, если вдруг были дубли и мы их почистили
     print("Keep scraping. No chart found yet.")
+
