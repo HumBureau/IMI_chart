779eca42c045ec3d9f45a2f0027654fa46caa5e0
HumBureau <70520141+HumBureau@users.noreply.github.com>
2020-11-11 15:09:00 +0300

    new deezer check if there's already this date's chart procedure

--- Daily/Deezer_daily_parsing.py
+++ Daily/Deezer_daily_parsing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[33]:
+# In[111]:
 
 
 import pandas as pd
@@ -12,44 +12,25 @@ from time import sleep
 from random import randint
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
-import json
+from os import path
 
 
-# In[69]:
+# In[147]:
 
 
-
-    
-    
-# базовая ссылка на последний актуальный ежедневный чарт по России
-request_deezer = requests.get('https://api.deezer.com/playlist/1116189381') # ссылка на постоянный плейлист
-deezer_chart_json = request_deezer.json() # через API получаем json 
-
-now = datetime.now()
-
-# проверяем, есть ли в API новые данные
-
-old_json = json.load(open('deezer_old_json.json', 'r'))
-
-if old_json["tracks"]["data"] == deezer_chart_json ["tracks"]["data"]:
-    print(now, ": Deezer chart API has not been updated yet. Will try again in 1 hour.")
-else:
-    print(now, ": Found new chart in Deezer API.")
-    
-    # сохраняем новый json
-    with open('deezer_old_json.json', 'w') as json_file:
-        json.dump(deezer_chart_json, json_file)
+def get_new_chart(js):
     
     # грузим данные за предыдущие дни
     all_deezer = pd.read_csv("all_deezer.csv")
     all_deezer = all_deezer.drop(all_deezer.columns[[0]], axis=1) # удаляем получающуюся после импорта лишнюю колонку 
 
     # на всякий случай чистим от дублей
-    all_deezer = all_deezer.drop_duplicates()
-    all_deezer.reset_index(inplace=True) 
-    all_deezer.drop(all_deezer.columns[[0]], axis=1, inplace=True)
+    #all_deezer = all_deezer.drop_duplicates()
+    #all_deezer.reset_index(inplace=True) 
+    #all_deezer.drop(all_deezer.columns[[0]], axis=1, inplace=True)
+    
 
-    new_df = pd.DataFrame(deezer_chart_json['tracks']['data']) # выбираем только список треков
+    new_df = pd.DataFrame(js['tracks']['data']) # выбираем только список треков
 
     # Находим имена ВСЕХ артистов для каждого трека через API трека
 
@@ -69,21 +50,61 @@ else:
     new_df['rank'] = new_df.reset_index().index +1 
     new_df = new_df[['rank', 'title', 'artist']]
 
-    n_l = list(new_df["title"])   
         
     # задаем дату
     date = datetime.now() 
     new_df["date"] = datetime.strftime(date,"%d/%m/%Y")  
             
-    # вписываем данные в csv
+    # вписываем данные в старый csv
     frames = [all_deezer, new_df]
     all_deezer = pd.concat(frames, sort=False)
+    
     # чистим
-    all_deezer.drop_duplicates(inplace=True)
+    #all_deezer.drop_duplicates(inplace=True)
     all_deezer.reset_index(inplace=True) 
     all_deezer.drop(all_deezer.columns[[0]], axis=1, inplace=True)
     all_deezer.to_csv("all_deezer.csv", encoding = "utf-8")
     
     print(date, ": New Deezer chart is saved to data. Length of data: ", len(new_df))
+
+
+# In[156]:
+
+
+# базовая ссылка на последний актуальный ежедневный чарт по России
+request_deezer = requests.get('https://api.deezer.com/playlist/1116189381') # ссылка на постоянный плейлист
+deezer_chart_json = request_deezer.json() # через API получаем json 
+check_df = pd.DataFrame(deezer_chart_json['tracks']['data'])[["id"]]
+
+now = datetime.now()
+
+# проверяем, есть ли в API новые данные
+
+if path.exists("deezer_check_df.csv"):
+    old_check_df = pd.read_csv("deezer_check_df.csv")
+    old_check_df = old_check_df.drop(old_check_df.columns[[0]], axis=1) 
+    
+    if check_df.equals(old_check_df):
+        print(now, ": Deezer chart API has not been updated yet. Will try again in 1 hour.")
+    else:
+        print(now, ": Found new chart in Deezer API.")
+        check_df.to_csv("deezer_check_df.csv")
         
+        get_new_chart(deezer_chart_json)
+        
+        
+else:
+    check_df.to_csv("deezer_check_df.csv")
+    print(now, ": No deezer_check_df.csv found. Created new one.")
+    
+    get_new_chart(deezer_chart_json)
+    
+    
+    
+
+
+# In[ ]:
+
+
+
 
