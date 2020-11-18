6f115f6ab659e4d58f885be55e2654f9cb8c4652
HumBureau <70520141+HumBureau@users.noreply.github.com>
2020-11-18 14:41:25 +0300

    same updates as for other "weekly" scripts

--- Daily/Make_weekly_sber.py
+++ Daily/Make_weekly_sber.py
@@ -37,7 +37,20 @@ import heapq
 currentDT = datetime.now() 
 
 
-# In[4]:
+# In[ ]:
+
+
+
+
+
+# In[5]:
+
+
+# округляем дату до ровно начала суток
+currentDT = datetime.strptime(datetime.strftime(currentDT, "%d/%m/%Y"), "%d/%m/%Y")
+
+
+# In[6]:
 
 
 # загружаем полные базы данных по всем ежедневным чартам
@@ -49,7 +62,7 @@ for i in all_charts:
     i.drop(i.columns[[0]], axis=1, inplace=True)
 
 
-# In[5]:
+# In[7]:
 
 
 # сделаем вспомогательные объекты для работы с датами
@@ -60,12 +73,6 @@ for i in range(1,8):
 
 date_start = currentDT - relativedelta(days=+7)
 date_end = currentDT - relativedelta(days=+1)
-
-
-# In[ ]:
-
-
-
 
 
 # In[8]:
@@ -96,23 +103,59 @@ def average(df):
             # how many dates are missing? 
             not_missing_dates = list(one_track_df["date"])
             n_of_m_days = 7 - len(not_missing_dates)
+            missing_days = list(set(all_dates) - set(not_missing_dates))
             
-            if n_of_m_days <0:
-                print("Found a song with > 7 appearances in the week. Taking 7 highest ranks. Track:", i)
-                average_rank = sum(heapq.nsmallest(7, list(one_track_df["rank"]))) / 7
+            # определяем, нет ли песни в чарте за день потому, что вообще чарта для этого дня нет
+            denominator = 7            
+            added_ranks = []
+            for day in missing_days:
+                # выясняем длину чарта в тот день
+                one_DAY_df = df[df["date"] == day]
+                if len(one_DAY_df) >0:
+                    # добавляем rank равный строчке после последней видимой
+                    added_ranks.append(len(one_DAY_df) + 1)  
+                    print("track ", i, "is not in chart on:", day)
+                    print("I am assigning rank = ", len(one_DAY_df) + 1)
+                else:
+                    # сокращаем делитель на 1 (т.к. такого дня нет в данных вообще)
+                    print("No chart for this day. Will change the demominator in the average rank formula.")
+                    print("Day: ", day)
+                    denominator = denominator - 1
+            
+            full_ranks = added_ranks
+            full_ranks.extend(list(one_track_df["rank"]))
+            
+            # наконец считаем среднюю строку за неделю    
+            if len(full_ranks) > denominator:
+                # так бывает, если в чарте есть и сингл, и альбом
+                print("Found a song with more appearances than # of saved charts in this week (including added added 'n+1' ranks). Taking the highest # ranks only. ", i )
+                average_rank = sum(heapq.nsmallest(7, full_ranks)) / denominator
             else:
-                # добавляем rank = 101 для отсутствующих дней
-                average_rank = (sum(one_track_df["rank"]) + (101*n_of_m_days)) / 7 
- 
+                average_rank = (sum(full_ranks)) / denominator  
+                 
             songs.append(i)
             artists.append(j)
             raw_rank.append(average_rank)
 
     data = {"raw_rank": raw_rank, "title": songs, "artist": artists}        
     new_chart = pd.DataFrame(data)
+    
+    ### Присуджаем "чистый" номер строчки 
+    
     new_chart.sort_values(by=['raw_rank'], inplace=True)
-
     new_chart['rank'] = new_chart.reset_index().index +1
+    
+    # если raw_rank равен, уравняем rank тоже.
+    prev_raw_r = 0
+    prev_r = 0 
+    for r in new_chart.iterrows():
+        if r[1]["raw_rank"] == prev_raw_r:
+            new_chart.at[r[0], "rank"]=prev_r
+        else:
+            # new "group". so update rank. 
+            prev_r = r[1]["rank"]
+        prev_raw_r = r[1]["raw_rank"]
+        
     new_chart.reset_index(inplace=True)
     week = datetime.strftime(date_start,"%d/%m/%y") + " - " + datetime.strftime(date_end,"%d/%m/%y")
     new_chart["week"] = week
@@ -168,22 +211,6 @@ for c in all_simple_charts:
 
 # загружаем все чарты, агрегированные за неделю
 all_sber_weekly = new_csv
-
-
-# чистим колонки для удобства
-#all_weekly_charts= [all_sber_weekly]
-#for i in all_weekly_charts:
-    #try:
-        #i.drop(i.columns[[0]], axis=1, inplace=True)
-        # add missing columns (if they are missing)
-        #for h in ["weeks_in_chart", "best_pos", "full_id", "delta_rank"]:
-            #if h in i.columns:
-                #7+9
-            #else:
-                #i[h]=None
-    #except:
-        #6+8
-    #i.drop_duplicates(inplace=True)
 
 
 # In[12]:
@@ -302,7 +329,6 @@ for ch in all_curr_week_charts:
     old_csv = pd.read_csv(name_of_weekly_chart)    # загружаем старые данные
     old_csv = old_csv.drop(old_csv.columns[[0]], axis=1) # удаляем получающуюся после импорта лишнюю колонку 
     
-    #old_csv = old_csv[:-len(ch)] # ВАЖНО: удаляем чарт этой недели, в котором еще нет новых метрик
     
     frames = [old_csv, ch]
     new_csv = pd.concat(frames, sort=False)
