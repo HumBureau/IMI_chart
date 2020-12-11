b8569fc11ed615e3a090ab0c1479162ce8ffa354
HumBureau <70520141+HumBureau@users.noreply.github.com>
2020-12-11 12:49:40 +0300

    ...

--- Daily/Make_weekly_apple.py
+++ Daily/Make_weekly_apple.py
@@ -16,7 +16,7 @@
 ## - сохраняет html и json с актуальным чартом
 
 
-# In[1]:
+# In[ ]:
 
 
 import pandas as pd
@@ -29,27 +29,7 @@ import datetime
 from datetime import datetime, date, time, timezone
 from dateutil.relativedelta import relativedelta
 import heapq
-
-
-# In[5]:
-
-
-r = requests.get("https://mail.ru")
-r.status_code
-
-
-# In[ ]:
-
-
-# задаем команду для получения даты
-currentDT = datetime.now() 
-
-
-# In[ ]:
-
-
-# округляем дату до ровно начала суток
-currentDT = datetime.strptime(datetime.strftime(currentDT, "%d/%m/%Y"), "%d/%m/%Y")
+from Make_weekly_charts_functions import average
 
 
 # In[ ]:
@@ -64,111 +44,6 @@ all_apple = pd.read_csv("all_apple.csv")
 all_charts= [all_apple]
 for i in all_charts:
     i.drop(i.columns[[0]], axis=1, inplace=True)
-
-
-# In[ ]:
-
-
-# сделаем вспомогательные объекты для работы с датами
-all_dates = []
-for i in range(1,8):
-    k = currentDT - relativedelta(days=+i)
-    all_dates.append(datetime.strftime(k, "%d/%m/%Y"))
-
-date_start = currentDT - relativedelta(days=+7)
-date_end = currentDT - relativedelta(days=+1)
-
-
-# In[ ]:
-
-
-# функция для получения недельного чарта через усреднение ежедневных
-# только для apple, deezer, VK
-
-
-def average(df):
-  
-    df['Datetime'] = [datetime.strptime(i, "%d/%m/%Y") for i in df["date"]]
-    
-    # take last week only
-
-    last_week_df = df[date_start <= df["Datetime"]]
-    last_week_df = last_week_df[last_week_df["Datetime"] <= date_end ]
-    df = last_week_df
-    
-    raw_rank = []
-    songs =[]
-    artists = []
-    for i in list(set(df["title"])):
-        newdf = df[df["title"]==i]
-        for j in list(set(newdf["artist"])):
-            one_track_df = newdf[newdf["artist"]==j]
-            # how many dates are missing? 
-            not_missing_dates = list(one_track_df["date"])
-            n_of_m_days = 7 - len(not_missing_dates)   
-            missing_days = list(set(all_dates) - set(not_missing_dates))
-            
-            # определяем, нет ли песни в чарте за день потому, что вообще чарта для этого дня нет
-            denominator = 7            
-            added_ranks = []
-            for day in missing_days:
-                # выясняем длину чарта в тот день
-                one_DAY_df = df[df["date"] == day]
-                if len(one_DAY_df) >0:
-                    # добавляем rank равный строчке после последней видимой
-                    added_ranks.append(len(one_DAY_df) + 1)  
-                    print("track ", i, "is not in chart on:", day)
-                    print("I am assigning rank = ", len(one_DAY_df) + 1)
-                else:
-                    # сокращаем делитель на 1 (т.к. такого дня нет в данных вообще)
-                    print("No chart for this day. Will change the demominator in the average rank formula.")
-                    print("Day: ", day)
-                    denominator = denominator - 1
-            
-            full_ranks = added_ranks
-            full_ranks.extend(list(one_track_df["rank"]))
-            
-            # наконец считаем среднюю строку за неделю    
-            if len(full_ranks) > denominator:
-                # так бывает, если в чарте есть и сингл, и альбом
-                print("Found a song with more appearances than # of saved charts in this week (including added added 'n+1' ranks). Taking the highest # ranks only. ", i )
-                average_rank = sum(heapq.nsmallest(7, full_ranks)) / denominator
-            else:
-                average_rank = (sum(full_ranks)) / denominator  
-                 
-            songs.append(i)
-            artists.append(j)
-            raw_rank.append(average_rank)
-
-    data = {"raw_rank": raw_rank, "title": songs, "artist": artists}        
-    new_chart = pd.DataFrame(data)
-    
-    ### Присуджаем "чистый" номер строчки 
-    
-    new_chart.sort_values(by=['raw_rank'], inplace=True)
-    new_chart['rank'] = new_chart.reset_index().index +1
-    
-    # если raw_rank равен, уравняем rank тоже.
-    prev_raw_r = 0
-    prev_r = 0 
-    for r in new_chart.iterrows():
-        if r[1]["raw_rank"] == prev_raw_r:
-            new_chart.at[r[0], "rank"]=prev_r
-        else:
-            # new "group". so update rank. 
-            prev_r = r[1]["rank"]
-        prev_raw_r = r[1]["raw_rank"]
-        
-    new_chart.reset_index(inplace=True)
-    week = datetime.strftime(date_start,"%d/%m/%y") + " - " + datetime.strftime(date_end,"%d/%m/%y")
-    new_chart["week"] = week
-    
-    new_chart = new_chart[['rank', 'title', 'artist', "week", "raw_rank"]]
-    
-    # округляем raw_rank
-    new_chart["raw_rank"] = round(new_chart["raw_rank"], 3)
-    
-    return new_chart
 
 
 # In[ ]:
@@ -199,7 +74,7 @@ for c in all_simple_charts:
     
     old_csv = old_csv.drop(old_csv.columns[[0]], axis=1) # удаляем получающуюся после импорта лишнюю колонку 
     frames = [old_csv, output_chart]
-    new_csv = pd.concat(frames, sort=False)
+    new_csv = pd.concat(frames, sort=False, ignore_index=True)
  
 
 
@@ -209,17 +84,6 @@ for c in all_simple_charts:
 
 
 all_apple_weekly = new_csv
-
-# чистим колонки для удобства
-#all_weekly_charts= [all_apple_weekly]
-#for i in all_weekly_charts:
-#    try:
-#        i.drop(i.columns[[0]], axis=1, inplace=True)
-        #for h in ["weeks_in_chart", "best_pos", "full_id", "delta_rank"]:
-            #i[h] = None
-#    except:
-#        6+8
-    #i.drop_duplicates(inplace=True)
 
 
 # In[ ]:
@@ -273,17 +137,15 @@ def metrics_delta(chart):
     if len(chart_dropped) == 0:
         chart_previous_week = chart.loc[chart['week'] == chart['week'].values[1]]
     else: chart_previous_week = chart_dropped.loc[chart_dropped['week'] == chart_dropped['week'].values[-1]]
-    
-    
+       
     chart_previous_week = chart_previous_week[['title', 'artist', 'rank']]
     # ! chart_upd - данные по последней неделе
     chart_upd = pd.merge(chart_last_week, chart_previous_week, how='left', on=['title', 'artist']) 
     chart_upd['delta_rank'] = (chart_upd['rank_y'] - chart_upd['rank_x']).astype('Int64') 
 
+        
     
-    
-    
-    #number of weeks in chart (use weeks_in_chart() function)
+    #### number of weeks in chart (use weeks_in_chart() function)
     chart_upd.drop("weeks_in_chart", 1, inplace = True) #avoid duplicates in columns
     chart_upd = pd.merge(chart_upd, weeks_in_chart(chart), how='left', on=['title', 'artist'])
     
