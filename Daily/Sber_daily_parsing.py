c9e1ddcbe65ee0c3d1e85d4866757e9800a08f4a
HumBureau <70520141+HumBureau@users.noreply.github.com>
2020-12-13 14:24:46 +0300

    added genres/labels scraping to sber. + changed overall parsing method from regex to conversion of javascript object to json

--- Daily/Sber_daily_parsing.py
+++ Daily/Sber_daily_parsing.py
@@ -22,6 +22,23 @@ from random import randint
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from os import path
+import json
+
+
+# In[ ]:
+
+
+def get_genre_label(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser') 
+    nd = soup.findAll('script', attrs={'id':'__NEXT_DATA__'})[0]
+    textValue = nd.contents[0]
+    jsonValue = '{%s}' % (textValue.partition('{')[2].rpartition('}')[0],)
+    value = json.loads(jsonValue)
+    genre = ", ".join([i['name'] for i in value['props']['pageProps']['release']['genres']])
+    label = value['props']['pageProps']['release']['label']['title']
+    
+    return genre, label
 
 
 # In[ ]:
@@ -38,15 +55,29 @@ r = requests.get(base_url)
 
 soup = BeautifulSoup(r.text, 'html.parser')
 s = str(soup)
-songs = [i.strip('"') for i in re.findall(r'"title":\s*(.*?)\s*\,', s)[1:101] ]
-songs = [" ".join(i.split("\xa0")) for i in songs]
-songs = [" & ".join(i.split(" \\u0026 ")) for i in songs]
+nd = soup.findAll('script', attrs={'id':'__NEXT_DATA__'})[0]
+textValue = nd.contents[0]
+jsonValue = '{%s}' % (textValue.partition('{')[2].rpartition('}')[0],)
+value = json.loads(jsonValue)
 
-artists = [i.strip('"') for i in re.findall(r'(?<="credits":")(.*?)(?=",)', s)[:100] ]
-artists = [" & ".join(i.split(" \\u0026 ")) for i in artists]
+# некрасивая и хрупкая строчка вызвана тем, что в словаре чарт лежит под ключом равным (возможно изменяемому) id плейлиста
+full_chart_data_list = list(value['props']['pageProps']['grid']['playlists'].items())[0][1]["tracks"]
 
+songs = [i["title"] for i in full_chart_data_list]
+artists = [i["credits"] for i in full_chart_data_list]
+release_ids = [str(i['release_id']) for i in full_chart_data_list]
 
-data = {"rank": [i for i in range(1, len(songs)+1)], "title": songs, "artist":artists}
+genres = []
+labels = []
+for i in release_ids:
+    url= 'https://sber-zvuk.com/release/'+i
+    outp = get_genre_label(url)
+    genres.append(outp[0])
+    labels.append(outp[1])
+
+cols = ["rank", "title", "artist", 'date', 'genre', 'label']
+
+data = dict(zip(cols, [[i for i in range(1, len(songs)+1)], songs, artists, [datetime.strftime(currentDT,"%d/%m/%Y") for i in range(1, len(songs)+1)],genres,labels]))
 sber = pd.DataFrame(data)
 
 
@@ -54,11 +85,6 @@ sber = pd.DataFrame(data)
 
 
 date = currentDT 
-sber["date"] = datetime.strftime(date,"%d/%m/%Y")  
-
-
-# In[ ]:
-
 
 # берем имеющийся csv файл и обновляем его
 if path.exists("all_sber.csv") == True:
@@ -77,7 +103,7 @@ if path.exists("all_sber.csv") == True:
         print(date, ": this date's SBER chart is not in our data yet. I proceed to save it.")
     
     frames = [all_sber, sber]
-    all_sber = pd.concat(frames, sort=False)
+    all_sber = pd.concat(frames, sort=False, ignore_index=True)
     all_sber.to_csv("all_sber.csv", encoding = "utf-8")
 else:
     sber.to_csv("all_sber.csv", encoding = "utf-8")
